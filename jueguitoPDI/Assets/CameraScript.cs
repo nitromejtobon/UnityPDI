a578b36c268b6cb4dce686209fbd3177858c904e
Nitromejtobon <nitromejtobon@hotmail.es>
2018-09-07 11:37:39 -0500

    joven

---
+++ jueguitoPDI/Assets/CameraScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraScript : MonoBehaviour
+{
+
+	static WebCamTexture backCam;
+
+	void Start()
+	{
+		if (backCam == null)
+			backCam = new WebCamTexture();
+
+		GetComponent<Image>().material.mainTexture = backCam;
+
+		if (!backCam.isPlaying)
+			backCam.Play();
+
+	}
+
+	void Update()
+	{
+
+	}
+}
